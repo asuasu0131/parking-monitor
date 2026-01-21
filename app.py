29e897b5627d72695144a9fac051466ef4840995
asuasu0131 <gin.gin.ginzi.0407@gmail.com>
2026-01-21 09:52:48 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -55,3 +55,4 @@ def get_layout():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     print(f"Starting server on 0.0.0.0:{port}")
+    socketio.run(app, host="0.0.0.0", port=port)
